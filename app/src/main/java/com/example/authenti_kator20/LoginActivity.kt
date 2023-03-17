995218f93d4f4a7ba77217044c073011cbe6dc18
Varun <varun.kashyap1207@gmail.com>
2023-03-18 00:39:17 +0530

    Validation

--- app/src/main/java/com/example/authenti_kator20/LoginActivity.kt
+++ app/src/main/java/com/example/authenti_kator20/LoginActivity.kt
@@ -1,12 +1,16 @@
 package com.example.authenti_kator20
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.core.content.ContextCompat
 import com.example.authenti_kator20.databinding.ActivityLoginBinding
 import com.example.authenti_kator20.databinding.ActivityMainBinding
+import com.jakewharton.rxbinding2.widget.RxTextView
 
+@SuppressLint("CheckResult")
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
@@ -16,6 +20,44 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//  Username Validation
+        val usernameStream = RxTextView.textChanges(binding.etEmail)
+            .skipInitialValue()
+            .map { username ->
+                username.isEmpty()
+            }
+        usernameStream.subscribe{
+            showTextMinimalAlert(it, "Email/Username")
+        }
+
+//  Password Validation
+        val passwordStream = RxTextView.textChanges(binding.etPassword)
+            .skipInitialValue()
+            .map { password ->
+                password.isEmpty()
+            }
+        passwordStream.subscribe{
+            showTextMinimalAlert(it, "Password")
+        }
+
+//  Button Enable True or False
+        val invalidFieldStream = io.reactivex.Observable.combineLatest(
+            usernameStream,
+            passwordStream
+        ) {usernameInvalid: Boolean, passwordInvalid: Boolean ->
+            !usernameInvalid && !passwordInvalid
+        }
+        invalidFieldStream.subscribe { isValid ->
+            if (isValid) {
+                binding.btnLogin.isEnabled = true
+                binding.btnLogin.backgroundTintList = ContextCompat.getColorStateList(this, R.color.primary_color)
+            } else {
+                binding.btnLogin.isEnabled = false
+                binding.btnLogin.backgroundTintList = ContextCompat.getColorStateList(this, android.R.color.darker_gray)
+            }
+        }
+
+//  Click
         binding.btnLogin.setOnClickListener{
             startActivity(Intent(this, HomeActivity::class.java))
         }
@@ -23,5 +65,12 @@ class LoginActivity : AppCompatActivity() {
         binding.tvHaventAccount.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
+    }
+
+    private fun showTextMinimalAlert(isNotValid: Boolean, text: String){
+        if (text == "Email/Username")
+            binding.etEmail.error = if (isNotValid) "$text Cannot be empty!" else null
+        else if (text == "Password")
+            binding.etPassword.error = if (isNotValid) "$text Cannot be empty!" else null
     }
 }
--- app/src/main/java/com/example/authenti_kator20/RegisterActivity.kt
+++ app/src/main/java/com/example/authenti_kator20/RegisterActivity.kt
@@ -1,11 +1,17 @@
 package com.example.authenti_kator20
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import androidx.core.content.ContextCompat
 import com.example.authenti_kator20.databinding.ActivityLoginBinding
 import com.example.authenti_kator20.databinding.ActivityRegisterBinding
+import com.jakewharton.rxbinding2.widget.RxTextView
+import java.util.Observable
 
+@SuppressLint("CheckResult")
 class RegisterActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegisterBinding
@@ -15,11 +21,107 @@ class RegisterActivity : AppCompatActivity() {
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+//  Full Name Validation
+        val nameStream = RxTextView.textChanges(binding.etFullname)
+            .skipInitialValue()
+            .map { name ->
+                name.isEmpty()
+            }
+        nameStream.subscribe {
+            showNameExistAlert(it)
+        }
+
+//  Email Validation
+        val emailStream = RxTextView.textChanges(binding.etEmail)
+            .skipInitialValue()
+            .map { email ->
+                !Patterns.EMAIL_ADDRESS.matcher(email).matches()
+            }
+        emailStream.subscribe{
+            showEmailValidAlert(it)
+        }
+
+//  Username Validation
+        val usernameStream = RxTextView.textChanges(binding.etUsername)
+            .skipInitialValue()
+            .map { username ->
+                username.length < 6
+            }
+        usernameStream.subscribe{
+            showTextMinimalAlert(it, "Username")
+        }
+
+//  Password Validation
+        val passwordStream = RxTextView.textChanges(binding.etPassword)
+            .skipInitialValue()
+            .map { password ->
+                password.length < 6
+            }
+        passwordStream.subscribe{
+            showTextMinimalAlert(it, "Password")
+        }
+
+//  Confirm Password Validation
+        val passwordConfirmStream = io.reactivex.Observable.merge(
+            RxTextView.textChanges(binding.etPassword)
+                .skipInitialValue()
+                .map { password ->
+                    password.toString() != binding.etConfirmPassword.text.toString()
+                },
+            RxTextView.textChanges(binding.etConfirmPassword)
+                .skipInitialValue()
+                .map { confirmPassword ->
+                    confirmPassword.toString() != binding.etPassword.text.toString()
+                })
+        passwordConfirmStream.subscribe {
+            showPasswordConfirmedAlert(it)
+        }
+
+//  Button Enable True or False
+        val invalidFieldStream = io.reactivex.Observable.combineLatest(
+            nameStream,
+            emailStream,
+            usernameStream,
+            passwordStream,
+            passwordConfirmStream
+        ) { nameInvalid: Boolean, emailInvalid: Boolean, usernameInvalid: Boolean, passwordInvalid: Boolean, passwordConfirmedInvalid: Boolean ->
+            !nameInvalid && !emailInvalid && !usernameInvalid && !passwordInvalid && !passwordConfirmedInvalid
+        }
+        invalidFieldStream.subscribe { isValid ->
+            if (isValid) {
+                binding.btnRegister.isEnabled = true
+                binding.btnRegister.backgroundTintList = ContextCompat.getColorStateList(this, R.color.primary_color)
+            } else {
+                binding.btnRegister.isEnabled = false
+                binding.btnRegister.backgroundTintList = ContextCompat.getColorStateList(this, android.R.color.darker_gray)
+            }
+        }
+
+//  Click
         binding.btnRegister.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
         binding.tvHaveAccount.setOnClickListener {
             startActivity(Intent(this, LoginActivity::class.java))
         }
+    }
+
+    private fun showNameExistAlert(isNotValid: Boolean){
+        binding.etFullname.error = if (isNotValid) "Name Cannot be Empty!" else null
+    }
+
+    private fun showTextMinimalAlert(isNotValid: Boolean, text: String){
+        if (text == "Username")
+            binding.etUsername.error = if (isNotValid) "$text Must be more than 6 letters!" else null
+        else if (text == "Password")
+            binding.etPassword.error = if (isNotValid) "$text Must be more than 8 letters!" else null
+    }
+
+    private fun  showEmailValidAlert(isNotValid: Boolean) {
+        binding.etEmail.error = if (isNotValid) "Invalid Email" else null
+    }
+
+    private fun showPasswordConfirmedAlert(isNotValid: Boolean){
+        binding.etConfirmPassword.error = if(isNotValid) "Please enter same passwords!" else null
     }
 }
