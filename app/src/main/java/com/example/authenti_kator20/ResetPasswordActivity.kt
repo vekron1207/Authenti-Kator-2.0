1aac2ea51872e0099df4d20493c0105cbb2834e1
Varun <varun.kashyap1207@gmail.com>
2023-03-21 16:24:43 +0530

    Forgot Password Changes

---
+++ app/src/main/java/com/example/authenti_kator20/ResetPasswordActivity.kt
@@ -0,0 +1,11 @@
+package com.example.authenti_kator20
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+    }
+}
