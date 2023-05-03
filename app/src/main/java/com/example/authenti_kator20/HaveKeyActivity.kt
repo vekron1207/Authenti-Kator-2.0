bb05482a7f0318fbfd4d3760f91bd8674cec9914
Varun <varun.kashyap1207@gmail.com>
2023-05-03 16:54:04 +0530

    BuggyUpdate

--- app/src/main/java/com/example/authenti_kator20/HaveKeyActivity.kt
+++ app/src/main/java/com/example/authenti_kator20/HaveKeyActivity.kt
@@ -4,16 +4,26 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.example.authenti_kator20.databinding.ActivityHaveKeyBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.jakewharton.rxbinding2.widget.RxTextView
+import kotlinx.android.synthetic.main.activity_have_key.authenticate_btn
 
 @SuppressLint("CheckResult")
 class HaveKeyActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityHaveKeyBinding
+    private lateinit var databaseRef: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHaveKeyBinding.inflate(layoutInflater)
+        databaseRef = FirebaseDatabase.getInstance("https://authenti-kator2-default-rtdb.firebaseio.com/").getReference("Nike_Db")
         setContentView(binding.root)
 
 //  Serial Key Validation
@@ -28,6 +38,32 @@ class HaveKeyActivity : AppCompatActivity() {
 
         binding.tvHaveKey.setOnClickListener {
             startActivity(Intent(this, HaveNotKeyActivity::class.java))
+        }
+
+        binding.authenticateBtn.setOnClickListener {
+            val serialNumber = binding.etSerialKey.text.toString().trim()
+            databaseRef.orderByChild("serial_number").equalTo(serialNumber).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        Log.d("HavekeyActivity", "Data from database: $snapshot")
+                        snapshot.children.forEach { productSnapshot ->
+                            val isAuthenticated = productSnapshot.child("isAuthenticated").value as Boolean
+                            val productType = productSnapshot.child("product_type").value as String
+                            if (isAuthenticated) {
+                                Toast.makeText(this@HaveKeyActivity, "Product already activated", Toast.LENGTH_SHORT).show()
+                            } else {
+                                databaseRef.child(productSnapshot.key!!).child("isAuthenticated").setValue(true)
+                                Toast.makeText(this@HaveKeyActivity, "Authentic $productType", Toast.LENGTH_SHORT).show()
+                            }
+                        }
+                    } else {
+                        Toast.makeText(this@HaveKeyActivity, "Fake product", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                override fun onCancelled(error: DatabaseError) {
+                    Log.e("HavekeyActivity", "Error:${error.message} ")
+                }
+            })
         }
     }
 
