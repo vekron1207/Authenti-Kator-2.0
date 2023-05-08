56e2302df7078cb652052af08265b951e6269665
Varun Kashyap <varun.kashyap1207@gmail.com>
2023-05-08 18:08:29 +0530

    Added same user authenticating again fuctionality

--- app/src/main/java/com/example/authenti_kator20/HaveKeyActivity.kt
+++ app/src/main/java/com/example/authenti_kator20/HaveKeyActivity.kt
@@ -7,10 +7,12 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import com.example.authenti_kator20.databinding.ActivityHaveKeyBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ServerValue
 import com.google.firebase.database.ValueEventListener
 import com.jakewharton.rxbinding2.widget.RxTextView
 import kotlinx.android.synthetic.main.activity_have_key.authenticate_btn
@@ -43,31 +45,42 @@ class HaveKeyActivity : AppCompatActivity() {
 
         binding.authenticateBtn.setOnClickListener {
             val serialNumber = binding.etSerialKey.text.toString().trim().toInt()
-            databaseRef.orderByChild("serial_number").equalTo(serialNumber.toDouble())
-                .addListenerForSingleValueEvent(object : ValueEventListener {
-                    override fun onDataChange(snapshot: DataSnapshot) {
-                        if (snapshot.exists()) {
-                            Log.d("HavekeyActivity", "Data from database: $snapshot")
-                            snapshot.children.forEach { productSnapshot ->
-                                val isAuthenticated = productSnapshot.child("isAuthenticated").value as Boolean
-                                Log.d("HavekeyActivity", "isAuthenticated value: $isAuthenticated")
-                                val productType = productSnapshot.child("product_type").value as String
-                                if (isAuthenticated) {
-                                    Toast.makeText(this@HaveKeyActivity, "Product already activated", Toast.LENGTH_SHORT).show()
-                                } else {
-                                    databaseRef.child(productSnapshot.key!!).child("isAuthenticated").setValue(true)
-                                    Toast.makeText(this@HaveKeyActivity, "Authentic $productType", Toast.LENGTH_SHORT).show()
+            val currentUser = FirebaseAuth.getInstance().currentUser?.uid
+            databaseRef.orderByChild("serial_number").equalTo(serialNumber.toDouble()).addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    if (snapshot.exists()) {
+                        Log.d("HavekeyActivity", "Data from database: $snapshot")
+                        snapshot.children.forEach { productSnapshot ->
+                            val isAuthenticated = productSnapshot.child("isAuthenticated").value as Boolean
+                            val productType = productSnapshot.child("product_type").value as String
+                            val authenticatedBy = productSnapshot.child("authenticated_by").value as? String
+                            val authenticatedAt = productSnapshot.child("authenticated_at").value as? Long
+                            if (isAuthenticated && authenticatedBy == currentUser) {
+                                // Product already authenticated by the current user
+                                Toast.makeText(this@HaveKeyActivity, "Product is real", Toast.LENGTH_SHORT).show()
+                            } else if (isAuthenticated && authenticatedBy != currentUser) {
+                                // Product already authenticated by another user
+                                Toast.makeText(this@HaveKeyActivity, "Fake product", Toast.LENGTH_SHORT).show()
+                            } else {
+                                // Product not yet authenticated
+                                databaseRef.child(productSnapshot.key!!).apply {
+                                    child("isAuthenticated").setValue(true)
+                                    child("authenticated_by").setValue(currentUser)
+                                    child("authenticated_at").setValue(ServerValue.TIMESTAMP)
                                 }
+                                Toast.makeText(this@HaveKeyActivity, "Authentic $productType", Toast.LENGTH_SHORT).show()
                             }
-                        } else {
-                            Toast.makeText(this@HaveKeyActivity, "Fake product", Toast.LENGTH_SHORT).show()
                         }
+                    } else {
+                        Toast.makeText(this@HaveKeyActivity, "Fake product", Toast.LENGTH_SHORT).show()
                     }
-                    override fun onCancelled(error: DatabaseError) {
-                        Log.e("HavekeyActivity", "Error:${error.message} ")
-                    }
-                })
+                }
+                override fun onCancelled(error: DatabaseError) {
+                    Log.e("HavekeyActivity", "Error:${error.message} ")
+                }
+            })
         }
+
     }
 
     private fun showTextMinimalAlert(isNotValid: Boolean, fieldName: String) {
